aef2c71e47437fe8c70ef085692106d01e58c8cb
joshuaditton@internode.on.net <joshuaditton@internode.on.net>
2023-10-13 19:36:55 +1100

    init

---
+++ assets/PTPerfectPage.16da92a0.js
@@ -0,0 +1,77 @@
+import{Q as _,P as c}from"./projects.b1c76436.js";import{_ as p,a as n}from"./ProjectPageCard.6443611d.js";import{y as l,s as o,t as a,u as e,v as i,A as r,C as t,B as g}from"./index.d82e378c.js";import"./dom.6a3d4960.js";import"./QBtn.54030acf.js";const w={class:"flex flex-center"},m=t("p",null,"This project was started sometime early 2020.",-1),d=["sizes"],f=["sizes"],u=["sizes"],h=t("figcaption",null,"Invitation Email",-1),v=["sizes"],x=t("figcaption",null,"Movement Patterns",-1),z=["sizes"],b=t("figcaption",null,"Equipment",-1),j=["sizes"],q=t("figcaption",null,"Equipment Show",-1),y=["sizes"],k=t("figcaption",null,"Equipment Edit",-1),E=["sizes"],B=t("figcaption",null,"Videos",-1),P=["sizes"],C=t("figcaption",null,"Videos Show",-1),S=["sizes"],V=t("figcaption",null,"Videos Edit",-1),T=["sizes"],D=t("figcaption",null,"Blueprints",-1),R=["sizes"],N=t("figcaption",null,"Blueprints Show",-1),Q=["sizes"],$=t("figcaption",null,"Regimen Edit",-1),A=["sizes"],I=t("figcaption",null,"Blueprints Clone Selection",-1),M=["sizes"],F=t("figcaption",null,"Clone Selection Dialog",-1),G=["sizes"],H=t("figcaption",null,"Clone Selection Dialog",-1),J=["sizes"],K=t("figcaption",null,"Clone Edit",-1),L=["sizes"],O=t("figcaption",null,"Edit Routine Dialog",-1),U=["sizes"],W=t("figcaption",null,"Routine Edit",-1),X=["sizes"],Y=t("figcaption",null,"Simplified DB Relations",-1),Z=["sizes"],ee=t("figcaption",null,"Backend Tests",-1),se=["sizes"],ae=l({__name:"PTPerfectPage",setup(te){return(ie,ne)=>(o(),a(_,null,{default:e(()=>[i(p,{project:r(c)},null,8,["project"]),t("div",w,[i(n,null,{heading:e(()=>[g(" git init ")]),description:e(()=>[m]),figure:e(({sizes:s})=>[t("img",{sizes:s,srcset:`
+              images/ptperfect/01-login_zjesup_c_scale,w_350.png   350w,
+              images/ptperfect/01-login_zjesup_c_scale,w_1000.png 1000w
+            `,src:"images/ptperfect/01-login_zjesup_c_scale,w_1000.png",alt:""},null,8,d)]),_:1}),i(n,null,{description:e(()=>[]),figure:e(({sizes:s})=>[t("img",{sizes:s,srcset:`
+              images/ptperfect/02-trainer-dash_sptzdt_c_scale,w_350.png   350w,
+              images/ptperfect/02-trainer-dash_sptzdt_c_scale,w_1000.png 1000w
+            `,src:"images/ptperfect/02-trainer-dash_sptzdt_c_scale,w_1000.png",alt:""},null,8,f)]),_:1}),i(n,null,{description:e(()=>[]),figure:e(({sizes:s})=>[t("img",{sizes:s,srcset:`
+              images/ptperfect/03-invite_toycih_c_scale,w_350.png   350w,
+              images/ptperfect/03-invite_toycih_c_scale,w_1000.png 1000w
+            `,src:"images/ptperfect/03-invite_toycih_c_scale,w_1000.png",alt:""},null,8,u)]),_:1}),i(n,null,{description:e(()=>[]),figure:e(({sizes:s})=>[h,t("img",{sizes:s,srcset:`
+              images/ptperfect/04-email_moarkr_c_scale,w_350.png 350w,
+              images/ptperfect/04-email_moarkr_c_scale,w_750.png 750w
+            `,src:"images/ptperfect/04-email_moarkr_c_scale,w_750.png"},null,8,v)]),_:1}),i(n,null,{description:e(()=>[]),figure:e(({sizes:s})=>[x,t("img",{sizes:s,srcset:`
+              images/ptperfect/06-movements_rznosq_c_scale,w_350.png   350w,
+              images/ptperfect/06-movements_rznosq_c_scale,w_1000.png 1000w
+            `,src:"images/ptperfect/06-movements_m5lxs9_c_scale,w_1000.png",alt:""},null,8,z)]),_:1}),i(n,null,{description:e(()=>[]),figure:e(({sizes:s})=>[b,t("img",{sizes:s,srcset:`
+              images/ptperfect/07-equipment-index_jf5ytn_c_scale,w_350.png   350w,
+              images/ptperfect/07-equipment-index_jf5ytn_c_scale,w_1000.png 1000w
+            `,src:"images/ptperfect/07-equipment-index_jf5ytn_c_scale,w_1000.png",alt:""},null,8,j)]),_:1}),i(n,null,{description:e(()=>[]),figure:e(({sizes:s})=>[q,t("img",{sizes:s,srcset:`
+              images/ptperfect/08-equipment-show_mtdksv_c_scale,w_350.png   350w,
+              images/ptperfect/08-equipment-show_mtdksv_c_scale,w_1000.png 1000w
+            `,src:"images/ptperfect/08-equipment-show_mtdksv_c_scale,w_1000.png",alt:""},null,8,y)]),_:1}),i(n,null,{description:e(()=>[]),figure:e(({sizes:s})=>[k,t("img",{sizes:s,srcset:`
+              images/ptperfect/09-equipment-edit_nh8fsl_c_scale,w_350.png   350w,
+              images/ptperfect/09-equipment-edit_nh8fsl_c_scale,w_1000.png 1000w
+            `,src:"images/ptperfect/09-equipment-edit_nh8fsl_c_scale,w_1000.png",alt:""},null,8,E)]),_:1}),i(n,null,{description:e(()=>[]),figure:e(({sizes:s})=>[B,t("img",{sizes:s,srcset:`
+              images/ptperfect/10-videos-index_h3ojuu_c_scale,w_350.png   350w,
+              images/ptperfect/10-videos-index_h3ojuu_c_scale,w_559.png   559w,
+              images/ptperfect/10-videos-index_h3ojuu_c_scale,w_724.png   724w,
+              images/ptperfect/10-videos-index_h3ojuu_c_scale,w_871.png   871w,
+              images/ptperfect/10-videos-index_h3ojuu_c_scale,w_1000.png 1000w
+            `,src:"images/ptperfect/10-videos-index_h3ojuu_c_scale,w_1000.png",alt:""},null,8,P)]),_:1}),i(n,null,{description:e(()=>[]),figure:e(({sizes:s})=>[C,t("img",{sizes:s,srcset:`
+              images/ptperfect/11-videos-show_nxqxw6_c_scale,w_350.png   350w,
+              images/ptperfect/11-videos-show_nxqxw6_c_scale,w_563.png   563w,
+              images/ptperfect/11-videos-show_nxqxw6_c_scale,w_726.png   726w,
+              images/ptperfect/11-videos-show_nxqxw6_c_scale,w_879.png   879w,
+              images/ptperfect/11-videos-show_nxqxw6_c_scale,w_1000.png 1000w
+            `,src:"images/ptperfect/11-videos-show_nxqxw6_c_scale,w_1000.png",alt:""},null,8,S)]),_:1}),i(n,null,{description:e(()=>[]),figure:e(({sizes:s})=>[V,t("img",{sizes:s,srcset:`
+              images/ptperfect/12-videos-edit_r3bcct_c_scale,w_350.png   350w,
+              images/ptperfect/12-videos-edit_r3bcct_c_scale,w_1000.png 1000w
+            `,src:"images/ptperfect/12-videos-edit_r3bcct_c_scale,w_1000.png",alt:""},null,8,T)]),_:1}),i(n,null,{description:e(()=>[]),figure:e(({sizes:s})=>[D,t("img",{sizes:s,srcset:`
+              images/ptperfect/13-blueprints-index_gvfhss_c_scale,w_350.png   350w,
+              images/ptperfect/13-blueprints-index_gvfhss_c_scale,w_1000.png 1000w
+            `,src:"images/ptperfect/13-blueprints-index_gvfhss_c_scale,w_1000.png",alt:""},null,8,R)]),_:1}),i(n,null,{description:e(()=>[]),figure:e(({sizes:s})=>[N,t("img",{sizes:s,srcset:`
+              images/ptperfect/14-blueprints-show_mfe05r_c_scale,w_350.png   350w,
+              images/ptperfect/14-blueprints-show_mfe05r_c_scale,w_582.png   582w,
+              images/ptperfect/14-blueprints-show_mfe05r_c_scale,w_755.png   755w,
+              images/ptperfect/14-blueprints-show_mfe05r_c_scale,w_1000.png 1000w
+            `,src:"images/ptperfect/14-blueprints-show_mfe05r_c_scale,w_1000.png",alt:""},null,8,Q)]),_:1}),i(n,null,{description:e(()=>[]),figure:e(({sizes:s})=>[$,t("img",{sizes:s,srcset:`
+              images/ptperfect/15-regimens-edit_rhllxv_c_scale,w_350.png   350w,
+              images/ptperfect/15-regimens-edit_rhllxv_c_scale,w_1000.png 1000w
+            `,src:"images/ptperfect/15-regimens-edit_rhllxv_c_scale,w_1000.png",alt:""},null,8,A)]),_:1}),i(n,null,{description:e(()=>[]),figure:e(({sizes:s})=>[I,t("img",{sizes:s,srcset:`
+              images/ptperfect/16-blueprints-clone-show_lawyct_c_scale,w_350.png   350w,
+              images/ptperfect/16-blueprints-clone-show_lawyct_c_scale,w_1000.png 1000w
+            `,src:"images/ptperfect/16-blueprints-clone-show_lawyct_c_scale,w_1000.png",alt:""},null,8,M)]),_:1}),i(n,null,{description:e(()=>[]),figure:e(({sizes:s})=>[F,t("img",{sizes:s,srcset:`
+              images/ptperfect/17-blueprints-clone-show-dialog_yn8omn_c_scale,w_350.png   350w,
+              images/ptperfect/17-blueprints-clone-show-dialog_yn8omn_c_scale,w_830.png   830w,
+              images/ptperfect/17-blueprints-clone-show-dialog_yn8omn_c_scale,w_1000.png 1000w
+            `,src:"images/ptperfect/17-blueprints-clone-show-dialog_yn8omn_c_scale,w_1000.png",alt:""},null,8,G)]),_:1}),i(n,null,{description:e(()=>[]),figure:e(({sizes:s})=>[H,t("img",{sizes:s,srcset:`
+              images/ptperfect/17-blueprints-clone-show-dialog_yn8omn_c_scale,w_350.png   350w,
+              images/ptperfect/17-blueprints-clone-show-dialog_yn8omn_c_scale,w_830.png   830w,
+              images/ptperfect/17-blueprints-clone-show-dialog_yn8omn_c_scale,w_1000.png 1000w
+            `,src:"images/ptperfect/17-blueprints-clone-show-dialog_yn8omn_c_scale,w_1000.png",alt:""},null,8,J)]),_:1}),i(n,null,{description:e(()=>[]),figure:e(({sizes:s})=>[K,t("img",{sizes:s,srcset:`
+              images/ptperfect/18-blueprints-clone-edit_exsddg_c_scale,w_350.png   350w,
+              images/ptperfect/18-blueprints-clone-edit_exsddg_c_scale,w_1000.png 1000w
+            `,src:"images/ptperfect/18-blueprints-clone-edit_exsddg_c_scale,w_1000.png",alt:""},null,8,L)]),_:1}),i(n,null,{description:e(()=>[]),figure:e(({sizes:s})=>[O,t("img",{sizes:s,srcset:`
+              images/ptperfect/19-regimen-show-dialog_z3kfsv_c_scale,w_350.png   350w,
+              images/ptperfect/19-regimen-show-dialog_z3kfsv_c_scale,w_1000.png 1000w
+            `,src:"images/ptperfect/19-regimen-show-dialog_z3kfsv_c_scale,w_1000.png",alt:""},null,8,U)]),_:1}),i(n,null,{description:e(()=>[]),figure:e(({sizes:s})=>[W,t("img",{sizes:s,srcset:`
+              images/ptperfect/20-routine-edit_jasal9_c_scale,w_350.png   350w,
+              images/ptperfect/20-routine-edit_jasal9_c_scale,w_644.png   644w,
+              images/ptperfect/20-routine-edit_jasal9_c_scale,w_1000.png 1000w
+            `,src:"images/ptperfect/20-routine-edit_jasal9_c_scale,w_1000.png",alt:""},null,8,X)]),_:1}),i(n,null,{description:e(()=>[]),figure:e(({sizes:s})=>[Y,t("img",{sizes:s,srcset:`
+              images/ptperfect/ptperfect_relations_simplified_bdsdk3_c_scale,w_200.jpg 200w,
+              images/ptperfect/ptperfect_relations_simplified_bdsdk3_c_scale,w_457.jpg 457w,
+              images/ptperfect/ptperfect_relations_simplified_bdsdk3_c_scale,w_656.jpg 656w,
+              images/ptperfect/ptperfect_relations_simplified_bdsdk3_c_scale,w_667.jpg 667w
+            `,src:"images/ptperfect/ptperfect_relations_simplified_bdsdk3_c_scale,w_667.jpg",alt:""},null,8,Z)]),_:1}),i(n,null,{description:e(()=>[]),figure:e(({sizes:s})=>[ee,t("img",{sizes:s,src:"images/ptperfect/21-tests_lyug0p.png",alt:""},null,8,se)]),_:1})])]),_:1}))}});export{ae as default};
---
+++ assets/PokerGameClockPage.a5a94ec5.js
@@ -0,0 +1,12 @@
+import{Q as i,b as c}from"./projects.b1c76436.js";import{_ as l,a as n}from"./ProjectPageCard.6443611d.js";import{_ as r,y as m,s as _,t as g,u as e,v as t,A as d,C as s,B as a}from"./index.d82e378c.js";import"./dom.6a3d4960.js";import"./QBtn.54030acf.js";const p={class:"flex flex-center"},k=["sizes"],h=["sizes"],u=["sizes"],f=m({__name:"PokerGameClockPage",setup(w){return(C,j)=>(_(),g(i,null,{default:e(()=>[t(l,{project:d(c)},null,8,["project"]),s("div",p,[t(n,null,{heading:e(()=>[a("Initial State")]),description:e(()=>[a(" This is how the game clock looks on load. All of the data is imported from the data.js file. This data.js file can be edited to your own needs, however, this game clock was built to be bundled with a desktop application. The desktop application dynamically sets the data files contents before launching the game clock. ")]),figure:e(({sizes:o})=>[s("img",{sizes:o,srcset:`
+              images/game-clock/start/Game-Clock_start_400.jpg   400w,
+              images/game-clock/start/Game-Clock_start_900.jpg   900w,
+              images/game-clock/start/Game-Clock_start_1200.jpg 1200w
+            `,src:"images/game-clock/start/Game-Clock_start_1200.jpg",alt:""},null,8,k)]),_:1}),t(n,null,{heading:e(()=>[a("Options Menu")]),description:e(()=>[a(" The options menu is opened and closed by clicking anywhere on game clock. The options allows for the manipulation of the timer, blinds up alarm and allows the tournament director to cycle through the current blindset and set which chips displayed. ")]),figure:e(({sizes:o})=>[s("img",{sizes:o,srcset:`
+              images/game-clock/options/Game-Clock_options_400.jpg   400w,
+              images/game-clock/options/Game-Clock_options_970.jpg   970w,
+              images/game-clock/options/Game-Clock_options_1200.jpg 1200w
+            `,src:"images/game-clock/options/Game-Clock_options_1200.jpg",alt:""},null,8,h)]),_:1}),t(n,null,{heading:e(()=>[a("A very long tournament")]),description:e(()=>[a(" If the tournament runs so long there are no longer any blinds remaining, the timer is component is removed and the last blind set is fixed. ")]),figure:e(({sizes:o})=>[s("img",{sizes:o,srcset:`
+              images/game-clock/end/Game-Clock_end_400.jpg   400w,
+              images/game-clock/end/Game-Clock_end_1200.jpg 1200w
+            `,src:"images/game-clock/end/Game-Clock_end_1200.jpg",alt:""},null,8,u)]),_:1})])]),_:1}))}});var P=r(f,[["__scopeId","data-v-fd78c140"]]);export{P as default};
